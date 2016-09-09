908354bef8119589b9a506c9b6b372a54d7a1d73
Matt Kime <matt@mattki.me>
2016-09-08 21:05:47 -0400

    remove unneeded line

--- complete_before_val.js
+++ complete_before_val.js
@@ -3,7 +3,5 @@ const Rx = require('rxjs');
 
 const readFile$ = Rx.Observable.bindNodeCallback(fs.readFile)
 
-const five$ = Rx.Observable.interval(100).take(5);
-
 readFile$('.git/HEAD')
 	.subscribe( val => console.log(val), () => {}, console.log('complete') );
